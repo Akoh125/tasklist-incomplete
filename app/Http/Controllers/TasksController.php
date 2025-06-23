47a7d5365ea6f3cd6631e46306d3086984a58217
akoh.yokohama <akoh.acu.ss12@gmail.com>
2025-06-23 20:32:45 +0900

    task model

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Task;    // 追加
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // タスク一覧を取得
+        $tasks = Task::all();         // 追加
+
+        // タスク一覧ビューでそれを表示
+        return view('tasks.index', [     // 追加
+            'tasks' => $tasks,        // 追加
+        ]);                                 // 追加
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
