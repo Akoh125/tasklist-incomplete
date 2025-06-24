4afffc3868444fe24d1860972293d950f394c3b6
akoh.yokohama <akoh.acu.ss12@gmail.com>
2025-06-24 15:35:37 +0900

    CRUD tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -22,51 +22,75 @@ class TasksController extends Controller
         ]);                                 // 追加
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+        // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
     public function create()
     {
-        //
+        $task = new Task;
+
+        // メッセージ作成ビューを表示
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+       // postでtasks/にアクセスされた場合の「新規登録処理」
     public function store(Request $request)
     {
-        //
+        // メッセージを作成
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+       // getでmessages/idにアクセスされた場合の「取得表示処理」
+    public function show($id)
     {
-        //
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ詳細ビューでそれを表示
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+        // getでmessages/id/editにアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
     {
-        //
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+
+        // メッセージ編集ビューでそれを表示
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    // putまたはpatchでtasks/idにアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
     {
-        //
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを更新
+        $task->content = $request->content;
+        $task->save();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    // deleteでmessages/idにアクセスされた場合の「削除処理」
+    public function destroy($id)
     {
-        //
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 }
